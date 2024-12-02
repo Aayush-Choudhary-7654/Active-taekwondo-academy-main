a7e626abdb0a2cc7d576a2a1af1bfdbc5252c59d
Aayush-Choudhary-7654 <opgaming7654@gmail.com>
2024-12-03 00:20:51 +0530

    modified:   components/ui/Navbar.tsx

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Link from "next/link";
 import ContactModal from './ContactModal';
 import { Menu, X } from 'lucide-react';
